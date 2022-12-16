01cc71bb186ce9db546ffe1d8cd9966cace8731d
tayjaf <tayyabx@gmail.com>
2022-12-16 01:30:02 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Tayyab Jafar on 2022-12-16.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
